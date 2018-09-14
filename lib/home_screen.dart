1e775c190dbac6395022a5480f30aded41aaf351
PoojaB26 <pbhaumik26@gmail.com>
2018-09-15 04:05:59 +0530

    completed home screen logic

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -23,53 +23,67 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    }
+  }
 
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.lightGreen,
-      appBar: AppBar(elevation: 0.0,),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
+        padding: const EdgeInsets.all(50.0),
+        child: ListView(
           children: <Widget>[
-            Text("MakeMyFlights",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 50.0,),
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Full Name",
-                  filled: true,
-                  fillColor: Colors.white
-              ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: AssetImage('assets/logo.png'),
+                  height: 150.0,
 
-            // onChanged listener listens to every change to textfield
+                ),
+                SizedBox(height: 20.0,),
+                Text("MakeMyFlights",
+                  style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height: 50.0,),
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderRadius:BorderRadius.circular(20.0)),
+                      hintText: "Full Name",
+                      filled: true,
+                      fillColor: Colors.grey[100],
+                    contentPadding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0)
+                  ),
+
+                  // onChanged listener listens to every change to textfield
 //              onChanged: (text){
 //                print(text);
 //              },
 
+                ),
+                SizedBox(height: 10.0,),
+                RaisedButton(
+                  padding: EdgeInsets.symmetric(horizontal: 100.0),
+                  child: new Text("Proceed"),
+                  color: Colors.greenAccent,
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0),
+                  ),
+                  onPressed: (){
+                    print(nameController.text);
+                  },
+
+                ),
+
+              ],
             ),
-            SizedBox(height: 50.0,),
-            MaterialButton(
-              height: 50.0,
-              minWidth: MediaQuery.of(context).size.width*0.90,
-              onPressed: (){
-                print(nameController.text);
-              },
-              child: Text("PROCEED", style: TextStyle(color: Colors.white),),
-              color: Colors.red,
-            )
           ],
-        ),
+        )
       ),
     );
   }
