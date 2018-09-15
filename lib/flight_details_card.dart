99495b1c9f35c95323feb061da75c5ce94f19a35
PoojaB26 <pbhaumik26@gmail.com>
2018-09-15 16:02:32 +0530

    new widget for flight details card

---
+++ lib/flight_details_card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class FlightDetailsCard extends StatelessWidget{
+
+  _cityStyle(String code, String cityName, String time){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Column(
+        children: <Widget>[
+          Text(code, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 44.0,),),
+          Text(cityName, style: TextStyle(fontSize: 14.0),),
+          SizedBox(height: 10.0,),
+          Text(time, style: TextStyle(color: Colors.grey, fontSize: 12.0),)
+        ],
+      ),
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5.0,
+      margin: EdgeInsets.all(0.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+      child: Container(
+        height: 120.0,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            _cityStyle("NYC", "New York City", "5:50 AM"),
+            Icon(Icons.airplanemode_active),
+            _cityStyle("SFO", "San Francisco", "8:50 AM"),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
